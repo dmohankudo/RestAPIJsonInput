155ef13c8dbb708ac48898de134f73476967a02c
Debaditya M <dmohankudo@users.noreply.github.com>
2017-05-03 07:35:40 +0530

    Update RestAPITestClient_git.py

--- RestAPITestClient_git.py
+++ RestAPITestClient_git.py
@@ -9,25 +9,19 @@ call all function using keywords -> helps to shuffle order
 why to use @proprties
 https://www.programiz.com/python-programming/property
 '''
-from logging import basicConfig, debug, DEBUG, error, info
-from  os import path, makedirs
-from requests import  Session
-from requests.packages.urllib3.exceptions import NewConnectionError, NewConnectionError
+from logging import basicConfig, DEBUG, info
+from os import path, makedirs
+from requests import Session
+# from requests.packages.urllib3.exceptions import NewConnectionError
 from time import ctime
 
-
-log_dir = 'LOG' 
+log_dir = 'LOG'
 if not path.isdir(log_dir):
     makedirs(log_dir)
 
 
-
-
-
-
-
 def compare_values(_value1, _value2):
-    return _value1==_value2, _value1, _value2
+    return _value1 == _value2, _value1, _value2
 
 
 class API(object):
@@ -35,7 +29,10 @@ class API(object):
        No function returns
     """
 
-    def __init__(self, _base_url, _client_cert, _api):
+    def __init__(self,
+                 _base_url,
+                 _client_cert,
+                 _api):
         '''
         @baseurl: https://xxxxxxxxxxxxxxxxxxxxx.xxx
         @client_cert : client cert in pem format
@@ -47,9 +44,7 @@ class API(object):
         logfile_name = 'APILog_' + _api + '_'
         logfile_name += ctime().replace(' ', '-').replace(':', '_')
         logfile_path = path.join(path.abspath(log_dir), logfile_name)
-
-        basicConfig(filename=logfile_path ,level=DEBUG)
-
+        basicConfig(filename=logfile_path, level=DEBUG)
         info('client cert is {cert}'.format(cert=self.client_cert))
 
     @property
@@ -67,59 +62,76 @@ class API(object):
 
     def create_session(self):
         self.session = Session()
- 
 
     def set_authentication_values(self):
         self.create_session()
         self.session.cert = self.client_cert
 
-    def post_request(self, _data_json):
+    def post_request(self,
+                     _data_json):
         self.set_authentication_values()
-        self.response = self.session.post(url=self.endpoint_url, json=_data_json)
+        try:
+            self.response = self.session.post(url=self.endpoint_url,
+                                              json=_data_json)
+        except Exception as e:
+            self.response = ''
+            info(e)
+        finally:
+            self.session.close()
 
-    def invoke_api(self, _data_json=None):
+    def invoke_api(self,
+                   _data_json=None):
         '''Call  api with test data, the last two argument not used yet'''
-        
-        info('post data is:{d}'.format(d=_data_json))
+        info('post data is:{data}'.format(data=_data_json))
         self.post_request(_data_json=self.tc_post_data)
-        info('response is: {r}-{code}'.format(r=self.response.json(), code=self.response.status_code))
+        info('response is: {r}-{code}'.format(r=self.response.json(),
+                                              code=self.response.status_code))
 
-    def verify_response(self, _status_code, _status_message, _tc_desc):
+    def verify_response(self,
+                        _status_code,
+                        _status_message,
+                        _tc_desc):
         ''' verify json respone from  '''
         resp_json = self.response.json()
         bool_stat1, bool_stat2 = None, None
         if _status_code:
             if 'statusCode' in resp_json:
-                bool_stat1, exp1, act1 = compare_values(_value1=str(_status_code), 
-                    _value2=str(resp_json['statusCode']))
-                info('{status}-{exp}-{act}'.format(status=bool_stat1,
-                                                            exp=exp1,
-                                                            act=act1))
+                (bool_stat1,
+                 exp1,
+                 act1) = compare_values(_value1=_status_code,
+                                        _value2=resp_json['statusCode'])
+                info('Both equal?->{status}-Expected->{exp}-Actual->{act}'.format(status=bool_stat1,
+                                                   exp=exp1,
+                                                   act=act1))
         if _status_message:
             if 'statusMessage' in resp_json:
                 bool_stat2, exp2, act2 = compare_values(_value1=_status_message, 
-                    _value2=resp_json['statusMessage'])
-                info('{status}-{exp}-{act}'.format(status=bool_stat2,
-                                                            exp=exp2,
-                                                            act=act2))
+                                                        _value2=resp_json['statusMessage'])
+                info('Both equal?->{status}-Expected->{exp}-Actual->{act}'.format(status=bool_stat2,
+                                                   exp=exp2,
+                                                   act=act2))
         if False in (bool_stat1, bool_stat2):
             self.tc_status = 'Fail'
-            error('TC Desc:{d}TC status:{s}'.format(s=self.tc_status,d=_tc_desc))
+            error('TC Desc:{d}TC status:{s}'.format(s=self.tc_status,
+                                                    d=_tc_desc))
         else:
             self.tc_status = 'Pass'
-            info('TC Desc:{d}TC status:{s}'.format(s=self.tc_status,d=_tc_desc))
+            info('TC Desc:{d}TC status:{s}'.format(s=self.tc_status,
+                                                   d=_tc_desc))
 
-
-    def testAPI(self, _data_json=None, _exp_statusCode=None, _exp_statusMessage=None):
-
+    def testAPI(self,
+                _data_json=None,
+                _exp_statusCode=None,
+                _exp_statusMessage=None):
+        info('-' * 40)
         self.tc_data = _data_json
         self.tc_desc = self.tc_data['desc']
         self.tc_post_data = self.tc_data['post_data']
+        self.tc_exp_result = self.tc_data['expected']
         info('TestCae Desc: {desc}'.format(desc=self.tc_desc))
         self.invoke_api(_data_json=self.tc_post_data)
-        if self.response.status_code in ( 200, 400):
-            self.verify_response(_status_code=_exp_statusCode, 
-            _status_message=_exp_statusMessage, 
-            _tc_desc=self.tc_desc)
+        if self.response.status_code in (200, 400, 415):
+            self.verify_response(_status_code=self.tc_exp_result['status_code'],
+                                 _status_message=self.tc_exp_result['status_message'],
+                                 _tc_desc=self.tc_desc)
         print('API call done...')
-
